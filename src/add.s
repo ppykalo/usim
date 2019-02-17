b4bde55d17a91fe8a883e1298999c3fce56433f0
ppykalo <ppykalo@users.noreply.github.com>
2019-02-17 13:52:58 +0100

    Add STR instruction to add.s

--- src/add.s
+++ src/add.s
@@ -1,3 +1,11 @@
 	mov r1, #1
 	mov r2, #2
 	add r2, r2, r1
+	ldr r3, =WRITE_VALUE
+	ldr r4, =WRITE_TARGET
+	str r3, [r4, #0]
+	bkpt #0
+
+	.equ WRITE_VALUE,	0xDEADBEEF
+	.equ WRITE_TARGET, 	0x20000000
+	
